43e5a414f100a12681a4a9d1c83ca6976412cfa2
1Bira <ubiratan.gleidson@gmail.com>
2024-06-25 09:03:06 +0100

    correction

--- src/codeScanning/GitHubCodeScanning.ts
+++ src/codeScanning/GitHubCodeScanning.ts
@@ -1,43 +1,57 @@
 import { Octokit } from '@octokit/rest';
-import { Repo, CodeScanningResults, CodeScanningAlert, CodeScanningData } from './types'; // Certifique-se de que estes tipos estão corretamente definidos e importados
-import * as core from '@actions/core';
+import { CodeScanningListAlertsForRepoResponseData, Endpoints } from '@octokit/types';
+import * as core from "@actions/core";
+import CodeScanningAlert, { CodeScanningData } from './CodeScanningAlert';
+import CodeScanningResults from './CodeScanningResults';
 
-interface ListCodeScanningAlertsParameters {
-  owner: string;
-  repo: string;
-  state: 'open' | 'fixed' | 'dismissed';
+type listCodeScanningAlertsParameters = Endpoints['GET /repos/:owner/:repo/code-scanning/alerts']['parameters'];
+
+type Repo = {
+  owner: string,
+  repo: string
 }
 
-interface CodeScanningListAlertsForRepoResponseData extends Array<CodeScanningData> {}
+export default class GitHubCodeScanning {
 
-async function getCodeScanning(
-  octokit: Octokit,
-  repo: Repo,
-  state: 'open' | 'fixed' | 'dismissed'
-): Promise<CodeScanningResults> {
-  const params: ListCodeScanningAlertsParameters = {
+  private readonly octokit: Octokit;
+
+  constructor(octokit) {
+    this.octokit = octokit;
+  }
+
+  getOpenCodeScanningAlerts(repo: Repo): Promise<CodeScanningResults> {
+    return getCodeScanning(this.octokit, repo, 'open');
+  }
+
+  getClosedCodeScanningAlerts(repo: Repo): Promise<CodeScanningResults> {
+    return getCodeScanning(this.octokit, repo, 'dismissed');
+  }
+}
+
+function getCodeScanning(octokit: Octokit,
+                         repo: Repo,
+                         state: 'open' | 'fixed' | 'dismissed'): Promise<CodeScanningResults> {
+
+  const params: listCodeScanningAlertsParameters = {
     owner: repo.owner,
     repo: repo.repo,
     state: state
   };
 
-  try {
-    const alerts: CodeScanningListAlertsForRepoResponseData = await octokit.paginate(
-      'GET /repos/:owner/:repo/code-scanning/alerts',
-      params
-    );
-
+  return octokit.paginate<CodeScanningListAlertsForRepoResponseData>(
+    'GET /repos/:owner/:repo/code-scanning/alerts',
+    params
+  ).then((alerts) => {
     core.info(`[✅] GET code-scanning.`);
-
     const results = new CodeScanningResults();
 
-    alerts.forEach((alert: CodeScanningData) => {
+    alerts.forEach((alert) => {
       results.addCodeScanningAlert(new CodeScanningAlert(alert));
     });
 
     return results;
-  } catch (err) {
+  }).catch((err) => {
     core.setFailed("[⚠️] There was an error in code-scanning. Please check the logs: " + err);
     throw err; // Lançar o erro após logar a falha
-  }
+  });
 }
